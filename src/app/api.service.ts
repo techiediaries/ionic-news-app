a62a59e0792af762e622149fec5de35c6afc2937
Nxtra <nickvanhoof1@gmail.com>
2019-12-16 21:29:59 +0100

    return Observable from getNews()

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs";
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -12,7 +12,7 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getNews(): Observable<any>{
-  return this.httpClient.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`);
+  getNews(): Observable<any> {
+    return this.httpClient.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`);
   }
 }
